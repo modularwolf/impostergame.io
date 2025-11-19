d570ba53288cbb19357f9c67e0e6dfc4226d4553
modularwolf <shadowtigerisme@gmail.com>
2025-11-19 13:35:17 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { supabase } from "./supabaseClient";
+import { supabase } from "./supabaseClient.ts";
 import "./style.css";
 
 // ---- Utility helpers ----
