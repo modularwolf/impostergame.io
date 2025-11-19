21381e11148f6fb292ce281e8e7fa816da20cfdf
modularwolf <shadowtigerisme@gmail.com>
2025-11-19 12:08:48 -0500

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import App from "./game";
+import App from "./App";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(container);
